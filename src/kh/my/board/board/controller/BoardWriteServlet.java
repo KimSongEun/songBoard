1ddefa7129a525b04bbb26e5998986673bf902bd
KimSongEun <songeun4939@naver.com>
2021-09-06 02:41:39 +0900

    새글과 답글 작성을 하나의 Controller를 사용하도록 수정하여 파일명 수정 후 코드 오류 수정

--- src/kh/my/board/board/controller/BoardWriteServlet.java
+++ src/kh/my/board/board/controller/BoardWriteServlet.java
@@ -15,16 +15,15 @@ import kh.my.board.board.model.vo.Board;
 /**
  * Servlet implementation class BoardReWriteServlet
  */
-@WebServlet("/boardwrite.kh")
-public class BoardReWriteServlet extends HttpServlet {
+@WebServlet("/boardwrite.se")
+public class BoardWriteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public BoardReWriteServlet() {
+    public BoardWriteServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
